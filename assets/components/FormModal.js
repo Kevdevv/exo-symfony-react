410bbdf34ce2d5baedbe44d8342f14c0f10acb09
Ramanantsoa Kevin <ramanantsoa.vlm@gmail.com>
2022-11-20 16:57:10 +0100

    d

--- assets/components/FormModal.js
+++ assets/components/FormModal.js
@@ -6,9 +6,9 @@ import Modal from 'react-bootstrap/Modal';
 const FormModal = () => {
 
     const [show, setShow] = useState(false);
-    console.log('a')
-    console.log('a')
-    console.log('a')
+    console.log('w')
+    console.log('w')
+    console.log('w')
     console.log('a')
 
 
