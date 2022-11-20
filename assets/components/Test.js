4f6ed7f5ced1b620d146dc04c2e5199205e97096
Ramanantsoa Kevin <ramanantsoa.vlm@gmail.com>
2022-11-20 16:39:49 +0100

    r

--- assets/components/Test.js
+++ assets/components/Test.js
@@ -1,8 +1,8 @@
 const Test = () => {
   const [show, setShow] = useState(false);
-  console.log('conflict')
-  console.log('conflict')
-  console.log('conflict')
-  console.log('conflict')
-  console.log('conflict')
+  console.log("conflict")
+  console.log("conflict")
+  console.log("conflict")
+  console.log("test")
+  console.log("test")
 }
